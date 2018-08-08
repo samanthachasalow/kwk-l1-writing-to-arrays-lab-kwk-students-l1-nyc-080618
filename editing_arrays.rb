871c4378f7d9af566ba3d882fd47840b5511ce36
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-08 18:24:43 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -5,6 +5,7 @@ def change_rainbow_colors
   rainbow_colors[0] = "red"
   rainbow_colors[1] = "light_red"
   rainbow_colors[2] = "light_yellow"
+  rainbow_colors
 end
 
 def add_colors
@@ -14,4 +15,5 @@ def add_colors
   rainbow_colors[2] = "light_yellow"
   rainbow_colors.push "green"
   rainbow_colors.push "blue"
+  rainbow_colors
 end
