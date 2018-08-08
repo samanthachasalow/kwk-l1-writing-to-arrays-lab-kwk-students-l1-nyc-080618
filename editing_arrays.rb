91334b578d9b12c7b95ee93349848e1341ff9763
samanthachasalow <samanthachasalow@chatham-nj.org>
2018-08-08 18:21:20 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -1,15 +1,15 @@
 rainbow_colors = ["yellow","default","light_cyan"]
 
 def change_rainbow_colors
-  rainbow_colors [0] = "red"
-  rainbow_colors [1] = "light_red"
-  rainbow_colors [2] = "light_yellow"
+  rainbow_colors[0] = "red"
+  rainbow_colors[1] = "light_red"
+  rainbow_colors[2] = "light_yellow"
 end
 
 def add_colors
-  rainbow_colors [0] = "red"
-  rainbow_colors [1] = "light_red"
-  rainbow_colors [2] = "light_yellow"
+  rainbow_colors[0] = "red"
+  rainbow_colors[1] = "light_red"
+  rainbow_colors[2] = "light_yellow"
   rainbow_colors.push "green"
   rainbow_colors.push "blue"
 end
